4501cd02a7c78b6a11390ec2a334cb7541a85ff9
Michael Denness <thisisturboproductions@gmail.com>
2024-02-19 15:02:32 -0500

    handleClick function

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  function handleClick() {
+    ()=>{ setFeatPupId(puppy.id)}
+  }
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
     </div>
   );
 }
